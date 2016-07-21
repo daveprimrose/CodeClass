ac5671af6e1d80488afc3bdebd2f7389a51e6458
daveprimrose <daveprim1993@googlemail.com>
2016-07-22 00:11:22 +0100

    completed StyleSheet1.css fixed minor errors in header.js
    contains bugs - file not found, see console

--- scripts/script.js
+++ scripts/script.js
@@ -31,6 +31,18 @@ function toggleHelp() {
     $("#help").animate({width: 'toggle'});
     var text = document.getElementById("toggleHelpButton").value;
 
+    if (text === "Show Help") {
+        document.getElementById("toggleHelpButton").value = "Hide Help";
+    }
+    else {
+        document.getElementById("toggleHelpButton").value = "Show Help";
+    }
+}
+
+function toggleInstructions() {
+    $("#instructions").animate({width: 'toggle'});
+    var text = document.getElementById("toggleInstructionsButton").value;
+
     if (text === "Show Instructions") {
         document.getElementById("toggleInstructionsButton").value = "Hide Instructions";
     }
@@ -38,6 +50,8 @@ function toggleHelp() {
         document.getElementById("toggleInstructionsButton").value = "Show Instructions";
     }
 }
+
+
 
 function getInput() {
     document.getElementById('console').innerHTML = "";
@@ -54,5 +68,106 @@ function getInput() {
     }
     if (isInput === true) {
         validate(myInput);
+    }
+}
+
+function validate(myInput) {
+    var result = JSLINT(myInput);
+    errorReport = JSLINT.data(result);
+    if(errorReport.errors.length !== 0) {
+        myReport = "At line " + (errorReport.errors[0].line - 2) + " character " + errorReport.errors[0].character + ":\n" + errorReport.errors[0].reason;
+        document.getElementById('console').value = myReport;
+    }
+    else {
+        document.getElementById('console').value = "The input is valid";
+    }
+}
+
+function updateUserIdentifier()
+{
+    var i;
+    userIdentifier = "";
+    characters = [];
+    for (i = 0; i < myInput[i].length; i++)
+    {
+        var charResult = myInput[i].indexOf("= new Character");
+        if(charResult != -1) {
+            var varResult = myInput[i].indexOf("var")
+            if(varResult != -1){
+                userIdentifier = (myInput[i].substr(4, charResult - 5));
+            }
+            else{
+                userIdentifier = (myInput[i].substr(0, charResult - 1));
+                characters.push(userIdentifier)
+            }
+        }
+    }
+    var insertsionButtons = document.getElementsByClassName("hButton");
+    if (typeof insertsionButtons ==="undefined") {
+        if (userIdentifier === "") {
+            for (var i = 0; i < insertsionButtons.length; i++) {
+                insertsionButtons[i].disabled = true;
+            }
+            insertsionButtons[insertsionButtons.length - 1].disabled = false;
+        }
+        else {
+            for (var i = 0; i < insertsionButtons.length; i++) {
+                insertsionButtons[i].disabled = false;
+            }
+        }
+    }
+}
+
+function log(msg){
+    if(document.getElementById('console').value === "The input is valid") {
+        document.getElementById('console').value = msg;
+    }
+    else {
+        document.getElementById('console').value = document.getElementById('console').value + "\n" + msg;
+    }
+}
+
+function sprompt(msg){
+    var userInput;
+    $.msgBox({
+        type : "prompt",
+        title: "Prompt",
+        inputs: [
+            {header: msg, type: "text", name: "userInput"},
+        ],
+        buttons: [
+            {value: "Confirm"},
+            {value: "Cancel"}
+        ],
+        success: function(result, values) {
+            if(result === "Confirm"){
+                var v;
+                $(values).each(function (index, input) {
+                    if(input.name === "userInput") {
+                        userInput = input.value;
+                    }
+                });
+                alert(userInput);
+                return userInput;
+            }
+        }
+    });
+}
+
+function salert(aTitle, aContent){
+    $.msgbox()({
+        title: aTitle,
+        aContent: aContent,
+        type: "info"
+    });
+}
+
+function sleep(milliseconds)
+{
+    var start = new Date().getTime();
+    for (var i = 0;i<1e7;i++) {
+        if((new Date().getTime() - start) > milliseconds){
+            break;
+        }
     }
 }
--- scripts/setupEditor.js
+++ scripts/setupEditor.js
@@ -3,14 +3,12 @@
  */
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
-editor.showPrintMargin(false);
-editor.highlightActiveLine(false);
+editor.getSession().setMode("ace/mode/javascript");
 session = editor.getSession();
 session.setMode("ace/mode/javascript");
-session.useWrapMode(true);
 
 session.on('change', function(e) {
-   getInput();
+    getInput();
     updateUserIdentifier();
     upload = document.getElementById('run');
     if (document.getElementById('console').value != "The input is valid") {
@@ -21,7 +19,61 @@ session.on('change', function(e) {
     }
 });
 
+editor.commands.addCommand({
+    name:'addMove',
+    bindKey: {win:'Ctrl-Alt-F', mac: 'Command-F'},
+    exec: function (editor) {
+        addForLoop();
+    }
+});
+addMaxEditor();
 
 function clearCode() {
     editor.value("");
 }
+
+function toggleFullScreen() {
+    var editorID, consoleID;
+    editorID = document.getElementById("editor");
+    var console = document.getElementById("console");
+    var buttonContainer = document.getElementById("console");
+    if(editorID != null) {
+        editorID.setAttribute('id', 'editorFullScreen');
+        editor.resize(true);
+        console.style.position = "absolute";
+        console.style.bottom= "5%";
+        console.style.zIndex = 5;
+        console.style.width = "60%";
+        console.style.height = "15%";
+        console.style.minWidth = "585px";
+        console.style.margin = "auto -21px";
+        console.style.border = "1px solid black";
+        document.getElementById("buttonContainer").setAttribute("id", "buttonContainerFullScreen");
+    }
+    else {
+        editorID = document.getElementById("editorFullScreen");
+        editorID.setAttribute('id', 'editor');
+        editor.resize(true);
+        console.removeAttribute("style");
+        document.getElementById("buttonContainerFullScreen").setAttribute("id", "buttonContainer");
+    }
+}
+
+function addMaxEditor() {
+    var editorElement = document.getElementById("editor");
+    var img = document.createElement("img");
+    img.setAttribute("src", "images/max.png");
+    img.setAttribute("id", "maxEditor");
+    img.style.top = 0;
+    img.style.right = "18px";
+    img.style.width = "25px";
+    img.style.height = "25px";
+    img.style.zIndex = 5;
+    img.style.position = "absolute";
+    editorElement.appendChild(img);
+    $("#maxEditor").click(function(){
+       toggleFullScreen();
+    });
+}
+
+
